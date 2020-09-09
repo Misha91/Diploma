ea1651f99da2318b82a273891409d39678da545a
Misha Ivanov <mikhail.ivanov@rocketmail.com>
2020-09-09 12:39:56 +0200

    repo clean up

--- PCL_test/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mikhail/Desktop/dev/Diploma/PCL_test/src
-# Build directory: /home/mikhail/Desktop/dev/Diploma/PCL_test/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("my_pcl_tutorial")
--- new/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mikhail/Desktop/dev/Diploma/new
-# Build directory: /home/mikhail/Desktop/dev/Diploma/new/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
